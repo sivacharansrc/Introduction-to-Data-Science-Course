f39711c391210f134b6003b407d506ae962f3e9f
sivacharansrc <sivacharansrc@gmail.com>
2019-02-18 20:10:05 -0600

    Predictive Modeling & its stages - Problem Defination, Hypothesis Generation added to the file

---
+++ Predictive Modeling and ML Basics.py
@@ -0,0 +1,55 @@
+# INTRODUCTION TO PREDICTIVE MODELING
+
+# Examples where predictive modeling can be useful
+# Movie recommendations based on gender, age, sex, past movies etc
+# Predicting the stock price etc
+# Predictive modeling is using past data to predict future data
+
+# Identifying factors responsible for Sales reduction - This is detective analysis rather than predictive modeling since
+# no future forecast is involved
+
+# TYPES OF PREDICTIVE MODELS
+# Supervised Learning - A definite target variable is available from the historical data which can be used to predict
+# future data Ex: House price prediction. The house price may be dependent on the locality, number of rooms, area etc
+
+# Un-Supervised Learning - No specific target variables are available. Data are segmented based on the commonalities
+# and the differences between each other. Ex: Google News grouping, segmentation of the customers etc
+
+# Types of Supervised Learning - Target Variables are Continuous values (Regression) vs Discrete Values (Classification)
+# UnSupervised Learning - Classic example is market segmentation.. Like segmenting customer base in to
+# High Salary - High Spend, High Salary - Low Spend, Low Salary - High Spend, Low Salary - Low Spend
+
+# STAGES OF PREDICTIVE MODELING
+
+# Process of Predictive Modeling can be divided in to six stages
+# 1 - Problem Definition
+# 2 - Hypothesis Generation
+# 3 - Data Extraction from all possible sources
+# 4 - Data Exploration and Transformation
+# 5 - Predictive Modeling
+# 6 - Model Deployment / Implementation
+
+# Problem Identification: Identifying the right problem, and formulating the problem mathematically
+# Bad Problem Statement: Want to improve the profitability of credit card customers
+# Reason: The above goal can be achieved in many different ways. Like, increasing the APR of the credit cards, or
+# having different APR and benefits for different customer segments, or identifying customers with low default rate
+# A problem statement should have a straight forward specific goal
+# Good Problem Statement: Want to predict the default rate of customer (All the above defined possibilities rely
+# on the customer default rate. Hence, predicting the default rate is more straight forward goal / prob. statement
+
+# Hypothesis Generation: Listing down all possible variables, that might influence problem objective
+
+# Let us analyze all the possible factors that might affect the above problem statement:
+# Income: Higher Income people might have lower default rate
+# Job Type: Person with a stable job might have a lower default rate
+# Credit History: Previous repayment behavior can have an impact on future payment behavior
+# Education: Educated people have better understanding of credit products, and may have lower default rate
+
+# Hypothesis Generation should be done before looking at the data to avoid any bias
+
+
+
+
+
+
+
