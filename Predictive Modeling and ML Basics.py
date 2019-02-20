8a8e05552c3981ada89c812a1aed4213d04f87eb
sivacharansrc <sivacharansrc@gmail.com>
2019-02-20 01:28:17 -0600

    univariate, bivariate, and missing values treatment updated

--- Predictive Modeling and ML Basics.py
+++ Predictive Modeling and ML Basics.py
@@ -118,3 +118,143 @@ plt.hist(df['Age'])
 
 pd.DataFrame.boxplot(df)
 plt.boxplot(df['Fare'], flierprops=dict(markerfacecolor='r', marker='D'))
+
+# Uni-variate analysis for categorical variables:
+# Some of the analysis that can be performed or everyone is interested in uni-variate analysis is:
+# Count - absolute frequency of each category in a categorical variable
+# Count% - proportion of different categories in a categorical variable expressed as %
+
+proj_dir = "C:/Users/sivac/Documents/Python Projects/Introduction to Data Science Course/"
+path = proj_dir + "Data Files/Predictive Modeling and Machine Learning/data.csv"
+
+import pandas as pd
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 100)
+pd.set_option('expand_frame_repr', False)
+
+df = pd.read_csv(path)
+
+# Similar to continuous variables, we can perform either tabular methods or graphical methods
+df['Sex'].value_counts()  # Frequency of the categorical variable
+df['Sex'].value_counts() / df.shape[0]  # Frequency % of the categorical variable
+df['Sex'].value_counts() / len(df['Sex'])  # Frequency % of the categorical variable
+
+df['Sex'].value_counts().plot.bar()  # Bar plot for frequency
+(df['Sex'].value_counts() / df.shape[0]).plot.bar()  # Bar plot for frequency %
+
+# Bi-variate analysis
+# When two variables are studied together to understand their relationship (or if any relationship exists)
+# Bi-variate analysis helps in prediction
+
+# Types of bi-variate analysis:
+# Continuous - Continuous variables
+import matplotlib.pyplot as plt
+
+plt.scatter(df['Age'], df['Fare'])
+df.plot.scatter('Age', 'Fare')
+
+
+# Analyzing the figure there is not much information from the two variables
+
+df['Age'].corr(df['Fare'])
+df.corr()
+# As figured from the scatter plot, there exists a very weak correlation between Age and Fare
+
+# Categorical - Continuous Analysis
+# Does the mean age of male different from that of female?
+
+df.groupby('Sex')['Age'].mean()
+df.groupby('Sex')['Age'].mean().plot.bar()
+
+import scipy.stats as stats
+
+(df['Age'][df['Sex'] == "male"]).std(ddof=1)
+(df['Age'][df['Sex'] == "female"]).std(ddof=1)
+
+stats.ttest_ind(df['Age'][df['Sex'] == "male"], df['Age'][df['Sex'] == "female"],  nan_policy='omit')
+# The 2 sample t test suggest that there is a significant difference between the mean age of males from females
+
+# Categorical - Categorical Analysis
+# Does the gender have any relation on survival rates?
+
+pd.crosstab(df['Sex'], df['Survived'])
+stats.chi2_contingency(pd.crosstab(df['Sex'], df['Survived']))
+# From the pvalue, it is evident that there is a significant difference between sex and survival rates
+# Note that when we have an exclusive list of observed, then we can use the chisquare instead of the contigency function
+
+# MISSING VALUES TREATMENT
+
+# Types of Missing Values
+#       Missing completely at random (MCAR) - missing values have no relation to the value iteself neither other values
+#               For instance, in a dataset with two columns Age and IQ, the missing values in IQ column have
+#               no relation with both Age or IQ
+
+#       Missing at Random (MAR) - missing values have some related with other variables present in the data
+#               For instance, if we have two columns Age, and IQ, then the IQ values are missing for any age < 55,
+#               then this type of missing values are called MAR
+
+#       Missing not at random (MNAR) - missing value have some sort of relation with the column itself
+#               For example, in a data set with two columns Age, and IQ, if all values of IQ less than 100 are
+#               missing, then they are called MNAR
+
+# Identifying missing values
+# describe - this is helpful for finding missing values only for continuous variable
+# Isull - can be used for both continuous and categorical variable
+
+
+# Treating Missing Values
+# Imputation
+#       Continuous - mean, median, mode,  and regression methods
+#       Categorical - mode, classification model
+
+# Deletion
+#       Row wise deletion - Delete the entire row
+#       Column wise deletion - Delete entire column
+
+# Deletion treatment results in loss of data. So, imputation is preferred over deletion, unless required
+
+proj_dir = "C:/Users/sivac/Documents/Python Projects/Introduction to Data Science Course/"
+path = proj_dir + "Data Files/Predictive Modeling and Machine Learning/data.csv"
+
+import pandas as pd
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 100)
+pd.set_option('expand_frame_repr', False)
+
+df = pd.read_csv(path)
+
+# Identify missing values of numerical variables
+df.shape
+df.describe()
+len(df[df['Age'].isnull()])
+
+# To identify all missing values in the data set
+df.isnull().sum()
+# From the above function, it is evident that we have missing values in age, cabin, and Embarked
+
+# Drop any rows with missing values
+df.dropna()
+df.dropna().shape
+df.dropna().isnull().sum()
+
+# Drop only those rows where all values are missing
+df.dropna(how='all').shape
+# There are no rows where all values are missing
+
+# Dropping any columns that have missing values
+df.dropna(axis=1).shape   # 3 columns that had missing values were deleted
+
+# Dropping columns that have all missing values
+df.dropna(axis=1, how='all').shape   # There are no columns with all missing values
+
+# Filling all na with zero
+df.fillna(0)
+
+# If the above action of filling na is to be done within the dataset, we use the argument inplace=T
+# df.fillna(0, inplace=True)
+
+# To impute a numerical variable with value other than zero
+df['Age'].fillna(df['Age'].mean())
+
+
+
