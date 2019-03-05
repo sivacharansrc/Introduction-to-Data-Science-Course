2766b0ed68078ab9ee1eb9554797857aaeea2679
sivacharansrc <sivacharansrc@gmail.com>
2019-03-05 02:19:49 -0600

    Fixed all missing values, and started with variable transformations

--- Classification Project/pyScript/classificationProject.py
+++ Classification Project/pyScript/classificationProject.py
@@ -2,6 +2,7 @@ import pandas as pd
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.max_rows', 100)
 pd.set_option('expand_frame_repr', False)
+import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 
@@ -17,11 +18,60 @@ df.isnull().sum()
 df.shape
 
 #2 The ID column is not going to be helpful in predictions. So, the column can be totally removed from the data set
-#3 The Application Receipt date can be further broken down to month and year, and can then be removed
-#4 Convert Applicant Birthdate and Manager Birthdate to Age
 
-
+#3 Analyzing the missing values:
 df_clean.isnull().sum()
+# Columns with missing values: Applicant_City_PIN (80), Applicant_Gender (53), Applicant_BirthDate (59), Applicant_Marital_Status (59), Applicant_Occupation (1090), Applicant_Qualification (71)
+
+# Analyzing whether applicant city PIN and Office PIN are same
+df_clean['Applicant_City_PIN'].nunique()
+df_clean['PIN_Match'] = np.where(df_clean['Office_PIN'] == df_clean['Applicant_City_PIN'], "Match", "No Match")
+df_clean.PIN_Match.value_counts()
+df_clean.drop('PIN_Match', axis=1, inplace=True)
+# Most of the PIN did not match, and so dropping the column
+
+# 53 missing values for both gender, and birth date. Checking whether both the variables having missing values in same rows, but this does not appear to be in the same row
+(df_clean['Applicant_Gender'][df_clean['Applicant_BirthDate'].isnull()]).isnull().value_counts()
+
+pd.crosstab(df_clean.Applicant_Gender, df_clean.Applicant_Occupation)
+
+#3a Let us fill the Applicant Gender with mode
+
+#3b Let us create a new column called Applicant_Age from the birthdate, fix outliers, and then fill missing values with median of the age
+df_clean['Applicant_Age_At_Joining'].plot.box()
+df_clean['Applicant_Age_At_Joining'].plot.hist()
+df_clean['Business_Sourced'][df_clean['Applicant_Age_At_Joining']>57.9].value_counts()
+#3b Let us not fix outliers for age, as the data is derieved from the birth date. Let us just impute the series with median
+
+#3c Let us analyze for a threshold age for married vs unmarried
+
+bins = [0, 10, 20, 25, 30, 40, 80]
+group = ["0-10", "10-20", "20-25", "25-30", "30-40", "40-80"]
+df_clean['Age_Group'] = pd.cut(df_clean['Applicant_Age'], bins, labels=group)
+
+pd.crosstab(df_clean['Age_Group'], df_clean.Applicant_Marital_Status)
+#3c Let us assume that age less than 25 are unmarried, and greater than 25 are married
+
+#3d Analyzing Applicant Occupation
+df_clean.Applicant_Occupation.value_counts()
+df_clean.loc[df_clean.Applicant_Occupation.isnull(), 'Age_Group'].value_counts()
+
+# After careful analysis, there is not any specific relation with other variables. Hence, let us create a new value for missing occupation as "Missing"
+
+#3e Applicant_Qualification - Fixing the factors, and missing values
+df_clean.Applicant_Qualification.value_counts()
+# Looking at the different qualifications, some of the qualifications such as the associate, and professional marketing can be re-categorized under Others
+pd.crosstab(df_clean.Age_Group, df_clean.Applicant_Qualification)
+df_clean.Applicant_Qualification.isnull().value_counts()
+# Let us fill the missing values with mode
+
+#4 Convert Manager Birthdate to Age at the time of recruiting the applicant using the Application Receipt Date
+
+#5 The Application Receipt date can be further broken down to month and year, and can then be removed
+
+
+
+
 
 
 
@@ -31,25 +81,42 @@ df_clean = df[~df['Manager_DOJ'].isnull()]
 #2 Removing the ID Column
 df_clean.drop('ID', axis=1, inplace=True)
 
-#3 Feature Engineering Application Recipt Date and Manager DOJ
+#3a Filling Applicant_Gender missing values with mode
+df_clean.loc[df_clean['Applicant_Gender'].isnull(), 'Applicant_Gender'] = df_clean['Applicant_Gender'].mode()[0]
+
+#3b Creating Applicant_Age from the birth date, and imputing missing data with median
+df_clean['Applicant_BirthDate'] = pd.to_datetime(df_clean['Applicant_BirthDate'])
 df_clean['Application_Receipt_Date'] = pd.to_datetime(df_clean['Application_Receipt_Date'])
-df_clean['Application_Receipt_Year'] = df_clean['Application_Receipt_Date'].dt.strftime("%Y")
-df_clean['Application_Receipt_Month'] = df_clean['Application_Receipt_Date'].dt.strftime("%m")
-df_clean.drop('Application_Receipt_Date', axis=1, inplace=True)
+df_clean['Applicant_Age_At_Joining'] = (df_clean['Application_Receipt_Date'] - df_clean['Applicant_BirthDate'])
+df_clean['Applicant_Age_At_Joining'] = df_clean['Applicant_Age_At_Joining'].dt.days / 365
+df_clean.loc[df_clean['Applicant_Age_At_Joining'].isnull(), 'Applicant_Age_At_Joining'] = df_clean['Applicant_Age_At_Joining'].median()
+df_clean['Applicant_Age_At_Joining'] = df_clean['Applicant_Age_At_Joining'].astype(int)
+
+#3c Filling Applicant_Marital_Status < 25 as S and > 25 as M
+
+df_clean.loc[(df_clean['Applicant_Age'] < 25) & (df_clean['Applicant_Marital_Status'].isnull()), 'Applicant_Marital_Status'] = "S"
+df_clean.loc[(df_clean['Applicant_Age'] >= 25) & (df_clean['Applicant_Marital_Status'].isnull()), 'Applicant_Marital_Status'] = "M"
+
+#3d Filling Applicant_Occupation missing values as missing
+
+df_clean.loc[df_clean['Applicant_Occupation'].isnull(), 'Applicant_Occupation'] = "Missing"
+
+#3e Fixing the Applicant_Qualification - Recategorizing,
+
+df_clean.loc[(~df_clean['Applicant_Qualification'].isin(['Class XII', 'Class X', 'Graduate', 'Masters of Business Administration'])) & (~df_clean['Applicant_Qualification'].isnull()), 'Applicant_Qualification'] = "Others"
+df_clean.loc[df_clean['Applicant_Qualification'].isnull(), 'Applicant_Qualification'] = df_clean['Applicant_Qualification'].mode()[0]
+
+
 
 #4 Converting Applicant and Manager Birthdate to Age instead of date (i.e. Age as of Jan 1 2009)
-df_clean['Applicant_BirthDate'] = pd.to_datetime(df_clean['Applicant_BirthDate'])
-x =  dt.datetime(2009, 1, 1,0,0)
-df_clean['Applicant_Age'] = (x - df_clean['Applicant_BirthDate'])
-df_clean['Applicant_Age'] = df_clean['Applicant_Age'].dt.days / 365
-
-# Manager DOB
 df_clean['Manager_DoB'] = pd.to_datetime(df_clean['Manager_DoB'])
-x =  dt.datetime(2009, 1, 1,0,0)
-df_clean['Manager_Age'] = (x - df_clean['Manager_DoB'])
-df_clean['Manager_Age'] = df_clean['Manager_Age'].dt.days / 365
+df_clean['Manager_Age_When_Recruiting'] = (df_clean['Application_Receipt_Date'] - df_clean['Manager_DoB'])
+df_clean['Manager_Age_When_Recruiting'] = df_clean['Manager_Age_When_Recruiting'].dt.days / 365
+df_clean.loc[df_clean['Manager_Age_When_Recruiting'].isnull(), 'Manager_Age_When_Recruiting'] = df_clean['Manager_Age_When_Recruiting'].median()
+df_clean['Manager_Age_When_Recruiting'] = df_clean['Manager_Age_When_Recruiting'].astype(int)
 
-df_clean.drop(['Manager_DoB', 'Applicant_BirthDate'], axis=1, inplace=True)
 
+#5 Feature Engineering Application Recipt Date and Manager DOJ
 
-df_clean.head()
+df_clean['Application_Receipt_Year'] = df_clean['Application_Receipt_Date'].dt.strftime("%Y").astype(int)
+df_clean['Application_Receipt_Month'] = df_clean['Application_Receipt_Date'].dt.strftime("%m").astype(int)
