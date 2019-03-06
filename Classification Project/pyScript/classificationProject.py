aed752e21a6a74c6da3d0cfe0ea344e0ca6570c3
sivacharansrc <sivacharansrc@gmail.com>
2019-03-05 21:04:09 -0600

    Priliminary Feature Engineering Completed

--- Classification Project/pyScript/classificationProject.py
+++ Classification Project/pyScript/classificationProject.py
@@ -69,6 +69,8 @@ df_clean.Applicant_Qualification.isnull().value_counts()
 
 #5 The Application Receipt date can be further broken down to month and year, and can then be removed
 
+#6a & 6b: The Office PIN and Applicant City PIN can be grouped in to respective PIN Zones. Reference for Indian PIN Zone: https://en.wikipedia.org/wiki/Postal_Index_Number
+
 
 
 
@@ -79,7 +81,7 @@ df_clean.Applicant_Qualification.isnull().value_counts()
 df_clean = df[~df['Manager_DOJ'].isnull()]
 
 #2 Removing the ID Column
-df_clean.drop('ID', axis=1, inplace=True)
+df_clean = df_clean.drop('ID', axis=1)
 
 #3a Filling Applicant_Gender missing values with mode
 df_clean.loc[df_clean['Applicant_Gender'].isnull(), 'Applicant_Gender'] = df_clean['Applicant_Gender'].mode()[0]
@@ -94,8 +96,8 @@ df_clean['Applicant_Age_At_Joining'] = df_clean['Applicant_Age_At_Joining'].asty
 
 #3c Filling Applicant_Marital_Status < 25 as S and > 25 as M
 
-df_clean.loc[(df_clean['Applicant_Age'] < 25) & (df_clean['Applicant_Marital_Status'].isnull()), 'Applicant_Marital_Status'] = "S"
-df_clean.loc[(df_clean['Applicant_Age'] >= 25) & (df_clean['Applicant_Marital_Status'].isnull()), 'Applicant_Marital_Status'] = "M"
+df_clean.loc[(df_clean['Applicant_Age_At_Joining'] < 25) & (df_clean['Applicant_Marital_Status'].isnull()), 'Applicant_Marital_Status'] = "S"
+df_clean.loc[(df_clean['Applicant_Age_At_Joining'] >= 25) & (df_clean['Applicant_Marital_Status'].isnull()), 'Applicant_Marital_Status'] = "M"
 
 #3d Filling Applicant_Occupation missing values as missing
 
@@ -120,3 +122,23 @@ df_clean['Manager_Age_When_Recruiting'] = df_clean['Manager_Age_When_Recruiting'
 
 df_clean['Application_Receipt_Year'] = df_clean['Application_Receipt_Date'].dt.strftime("%Y").astype(int)
 df_clean['Application_Receipt_Month'] = df_clean['Application_Receipt_Date'].dt.strftime("%m").astype(int)
+df_clean['Manager_DOJ'] = pd.to_datetime(df_clean['Manager_DOJ'])
+df_clean['Manager_DOJ_Year'] = df_clean['Manager_DOJ'].dt.strftime("%Y").astype(int)
+df_clean['Manager_DOJ_Month'] = df_clean['Manager_DOJ'].dt.strftime("%m").astype(int)
+
+#6a Feature Engineering Applicant City PIN
+df_clean['Applicant_City_Zone'] = 'Zone ' + df_clean['Applicant_City_PIN'].apply(str).str[0:1]  # Note that when Nan is converted to String, and the first letter is subset, it literally gets stored as n
+df_clean['Applicant_City_Zone'].replace('Zone n', np.nan, inplace=True)
+df_clean.loc[df_clean['Applicant_City_Zone'].isnull(), 'Applicant_City_Zone'] = df_clean['Applicant_City_Zone'].mode()[0]
+
+#6b Feature Engineering Office PIN
+df_clean['Office_PIN_Zone'] = 'Zone ' + df_clean['Office_PIN'].apply(str).str[0:1]  # Note that when Nan is converted to String, and the first letter is subset, it literally gets stored as n
+df_clean['Office_PIN_Zone'].replace('Zone n', np.nan, inplace=True)
+df_clean.loc[df_clean['Office_PIN_Zone'].isnull(), 'Office_PIN_Zone'] = df_clean['Office_PIN_Zone'].mode()[0]
+
+# Removing all unwanted columns
+df_prep = df_clean[['Applicant_Gender', 'Applicant_Marital_Status', 'Applicant_Occupation', 'Applicant_Qualification', 'Manager_Joining_Designation', 'Manager_Current_Designation', 'Manager_Grade', 'Manager_Status', 'Manager_Gender',
+					 'Manager_Num_Application', 'Manager_Num_Coded', 'Manager_Business', 'Manager_Num_Products', 'Manager_Business2', 'Manager_Num_Products2', 'Business_Sourced', 'Applicant_Age_At_Joining', 'Manager_Age_When_Recruiting',
+					 'Application_Receipt_Year', 'Application_Receipt_Month', 'Applicant_City_Zone', 'Office_PIN_Zone', 'Manager_DOJ_Year', 'Manager_DOJ_Month']]
+
+df_prep.head(20)
