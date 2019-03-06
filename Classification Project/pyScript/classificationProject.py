7616ec4a02501fc16eb387b7487a00669736e061
sivacharansrc <sivacharansrc@gmail.com>
2019-03-06 02:26:06 -0600

    all code required for running model added. Further tuning necessary

--- Classification Project/pyScript/classificationProject.py
+++ Classification Project/pyScript/classificationProject.py
@@ -5,6 +5,10 @@ pd.set_option('expand_frame_repr', False)
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+import statsmodels.api as sm
 
 # https://datahack.analyticsvidhya.com/contest/introduction-to-data-science-classification/
 
@@ -136,9 +140,118 @@ df_clean['Office_PIN_Zone'] = 'Zone ' + df_clean['Office_PIN'].apply(str).str[0:
 df_clean['Office_PIN_Zone'].replace('Zone n', np.nan, inplace=True)
 df_clean.loc[df_clean['Office_PIN_Zone'].isnull(), 'Office_PIN_Zone'] = df_clean['Office_PIN_Zone'].mode()[0]
 
+#7 Feature Engineering the column: Manager_Years_In_Company
+df_clean['Manager_Years_In_Company'] = df_clean['Application_Receipt_Date'] - df_clean['Manager_DOJ']
+df_clean['Manager_Years_In_Company']  = df_clean['Manager_Years_In_Company'] .dt.days / 365
+
+#7 Feature Engineering the column: Designation_Change
+df_clean['Designation_Change'] = np.where(df_clean.Manager_Joining_Designation != df_clean.Manager_Current_Designation,1,0)
+
 # Removing all unwanted columns
 df_prep = df_clean[['Applicant_Gender', 'Applicant_Marital_Status', 'Applicant_Occupation', 'Applicant_Qualification', 'Manager_Joining_Designation', 'Manager_Current_Designation', 'Manager_Grade', 'Manager_Status', 'Manager_Gender',
 					 'Manager_Num_Application', 'Manager_Num_Coded', 'Manager_Business', 'Manager_Num_Products', 'Manager_Business2', 'Manager_Num_Products2', 'Business_Sourced', 'Applicant_Age_At_Joining', 'Manager_Age_When_Recruiting',
-					 'Application_Receipt_Year', 'Application_Receipt_Month', 'Applicant_City_Zone', 'Office_PIN_Zone', 'Manager_DOJ_Year', 'Manager_DOJ_Month']]
+					 'Application_Receipt_Year', 'Application_Receipt_Month', 'Applicant_City_Zone', 'Office_PIN_Zone', 'Manager_DOJ_Year', 'Manager_DOJ_Month', 'Manager_Years_In_Company', 'Designation_Change']]
 
-df_prep.head(20)
+
+# Analyzing the data set
+
+# Checking whether the classes are balanced or not
+df_prep.Business_Sourced.value_counts() / len(df_prep)
+# There exists a class imbalance. We need to balance the data before we run the model
+
+# Let us analyze the correlation with the different variables and the target variable
+
+# Relation with application gender:
+pd.crosstab(df_prep.Applicant_Gender, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Applicant_Marital_Status, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Applicant_Occupation, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Applicant_Qualification, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Manager_Grade, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Manager_Status, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Manager_Gender, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Office_PIN_Zone, df_prep.Business_Sourced).plot.bar()
+
+
+pd.crosstab(df_prep.Applicant_Age_At_Joining, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Manager_Age_When_Recruiting, df_prep.Business_Sourced).plot.bar()
+pd.crosstab(df_prep.Application_Receipt_Month, df_prep.Business_Sourced).plot.bar()
+df_prep.Manager_Business.corr(df_prep.Business_Sourced)
+pd.scatter_matrix(df_prep)
+
+# Summary of columns that might have an impact on the target variable
+keep = ['Applicant_Gender', 'Applicant_Occupation', 'Applicant_Qualification', 'Manager_Grade', 'Manager_Gender', 'Applicant_Age_At_Joining', 'Manager_Age_When_Recruiting', 'Designation_Change', 'Applicant_City_Zone', 'Office_PIN_Zone',
+ 'Manager_Years_In_Company', 'Manager_Num_Application', 'Manager_Num_Coded', 'Manager_Business', 'Manager_Num_Products', 'Manager_Business2', 'Manager_Num_Products2', 'Business_Sourced']
+
+df_final = df_prep[keep]
+df_final = pd.get_dummies(df_final)
+
+# Reference: https://towardsdatascience.com/building-a-logistic-regression-in-python-step-by-step-becd4d56c9c8
+
+# Creating the Train and Test Data
+
+x = df_final.drop('Business_Sourced', axis=1)
+y = df_final['Business_Sourced']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, stratify=y)
+
+# Initiate a logistic regression object
+log_reg = LogisticRegression()
+
+rfe = RFE(log_reg, 15)
+rfe = rfe.fit(x_train, y_train.values.ravel())
+print(rfe.support_)
+print(rfe.ranking_)
+
+col_names = x_train.columns
+rfe_output = rfe.ranking_
+colToSelect = pd.DataFrame({'cols': col_names, 'rfe': rfe_output})
+colToSelect = colToSelect.loc[colToSelect.rfe == 1, 'cols']
+colToSelect = pd.Series.tolist(colToSelect)
+x_train = x_train[colToSelect]
+
+# Running Stats Model on the Selected Features
+
+logistic_model = sm.Logit(y_train, x_train)
+result = logistic_model.fit()
+print(result.summary2())
+
+stat_sig_col = ['Applicant_Occupation_Others', 'Applicant_Occupation_Salaried','Applicant_Occupation_Student', 'Applicant_Qualification_Class X','Applicant_Qualification_Class XII', 'Applicant_City_Zone_Zone 1',
+				'Applicant_City_Zone_Zone 2', 'Office_PIN_Zone_Zone 3']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, stratify=y, test_size=0.2)
+
+x_train = x_train[stat_sig_col]
+x_test = x_test[stat_sig_col]
+
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+
+# Predicting the results
+
+y_pred = logreg.predict(x_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(x_test, y_test)))
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+
+# ROC Curve
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(x_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(x_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
