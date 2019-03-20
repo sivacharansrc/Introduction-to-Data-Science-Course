c2f931a1ff7d09f7c07572de8bc535a44e09fff3
sivacharansrc <sivacharansrc@gmail.com>
2019-03-20 00:26:04 -0500

    added gridSearchCV optimizer

---
+++ Regression Project/HyperParameter Tuning.py
@@ -0,0 +1,36 @@
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from pprint import pprint
+import numpy as np
+
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt', 'log2']
+# Maximum number of levels in tree
+max_depth = [5,10,15,20,30,40,50,60,70,80,90,100]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestRegressor()
+# Random search of parameters, using 3 fold cross validation,
+# search across 100 different combinations, and use all available cores
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=1, n_jobs = -1)
+# Fit the random search model
+rf_random.fit(x_train, y_train)
+rf_random.best_params_
